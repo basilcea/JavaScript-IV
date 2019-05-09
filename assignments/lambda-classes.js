945e2b619c68b4e2eec9c9395664bd907ca15c66
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-09 12:59:12 +0100

    create person and student constructors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+        let [name ,age , location, gender] = attributes
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, i am from ${this.location},`
+    }
+}
+class Instructor extends Person {
+    constructor(attributes){
+        let[specialty , favLanguage , catchPhrase] = attributes;
+        super(attributes)
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    };
+    grade(student ,subject){
+        return `${student.name} receives a perfect score on ${subject}`
+
+    }
+
+}
