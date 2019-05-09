b297221b9010622d12df04fba53b203e2a157f38
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-09 14:21:49 +0100

    test class and class methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,7 +1,7 @@
 // CODE here for your Lambda Classes
 class Person {
     constructor(attributes){
-        let [name ,age , location, gender] = attributes
+        let {name ,age , location, gender} = attributes
         this.name = name;
         this.age = age;
         this.location = location;
@@ -13,7 +13,7 @@ class Person {
 }
 class Instructor extends Person {
     constructor(attributes){
-        let[specialty , favLanguage , catchPhrase] = attributes;
+        let {specialty , favLanguage , catchPhrase} = attributes;
         super(attributes)
         this.specialty = specialty;
         this.favLanguage = favLanguage;
@@ -30,7 +30,7 @@ class Instructor extends Person {
 }
 class Student extends Person{
     constructor(attributes){
-        let[previousBackground, className , favSubjects] = attributes;
+        let{previousBackground, className , favSubjects} = attributes;
         super(attributes)
         this.previousBackground = previousBackground;
         this.className = className;
@@ -40,10 +40,105 @@ class Student extends Person{
         this.favSubjects.forEach(subject => console.log(subject))
     };
     PRAssignment(subject){
-        console.log(`${student.name} has submitted a PR for ${subject}`)
+        console.log(`${this.name} has submitted a PR for ${subject}`)
 
     };
-    sprintChallenge(){
-        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge on ${subject}`)
     };
 }
+class ProjectManager extends Instructor{
+    constructor(attributes){
+        let {gradClassName ,favInstructor} = attributes;
+        super(attributes);
+        this.gradClassName = gradClassName;
+        this.favInstructor = favInstructor;
+    };
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
+
+    }
+    debugsCode(student ,subject){
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`
+        )
+    }
+}
+
+const firstPerson = new Person ({
+    name: 'firstPerson',
+    age: 13,
+    gender: "M",
+    location:'Nigeria'
+
+})
+const secondPerson = new Person ({
+    name: 'secondPerson',
+    age: 25,
+    gender: "F",
+    location:'Italy'
+
+})
+const firstInstructor = new Instructor ({
+    name: 'firstInstructor',
+    age: 35,
+    gender: "M",
+    location:'USA',
+    speciality: 'Frontend',
+    favLanguage:'Javascript',
+    catchPhrase:'I found peace at the node being charged with electron'
+})
+const secondInstructor = new Instructor ({
+    name: 'secondInstructor',
+    age: 22,
+    gender: "F",
+    location:'Russia',
+    speciality: 'Backend',
+    favLanguage:'Python',
+    catchPhrase:'Remember if its python, it must be robust'
+})
+const firstStudent = new Student ({
+    name: 'firstStudent',
+    age: 29,
+    gender: "M",
+    location:'Nigeria',
+    previousBackground: 'Telecoms',
+    className:'WEBEU2',
+    favSubjects:['HTML', 'CSS', 'Javascript','Python', 'Jupyter']
+})
+const secondStudent = new Student ({
+    name: 'secondStudent',
+    age: 22,
+    gender: "F",
+    location:'Russia',
+    previousBackground: 'Microbiology',
+    className:'WEBEU2',
+    favSubjects:['HTML','CSS','Javscript']
+})
+const firstPM = new ProjectManager ({
+    name: 'firstInstructor',
+    age: 30,
+    gender: "F",
+    location:'USA',
+    speciality: 'Devops',
+    favLanguage:'Python',
+    catchPhrase:'operation must be seamless',
+    gradClassName:'WEBEU1',
+    favInstructor:'secondInstructor'
+})
+const secondPM = new ProjectManager ({
+    name: 'secondInstructor',
+    age: 23,
+    gender: "F",
+    location:'Denmark',
+    speciality: 'Quality Assurance',
+    favLanguage:'Ruby',
+    catchPhrase:'I think i saw ruby, he is playing on the rails',
+    gradClassName:'WEBEU1',
+    favInstructor:'firstInstructor'
+})
+console.log(firstPerson)
+console.log(secondPerson.speak())
+console.log(firstInstructor.demo('javascript'))
+console.log(secondInstructor.grade(firstStudent,'javascript'))
+console.log(secondStudent.listsSubjects())
+console.log(secondStudent.PRAssignment('javascript'))
